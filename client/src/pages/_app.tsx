3f50e23bb9a1dd76a7d71872665d4ea47b3f85eb
DevNikita16 <83761318+DevNikita16@users.noreply.github.com>
2022-01-27 23:20:08 +0100

    Update _app.tsx

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -4,7 +4,7 @@ import Layout from '../components/Layout'
 
 import '../styles/app.scss'
 
-Axios.defaults.baseURL = 'https://messenger.tequiladev.space'
+Axios.defaults.baseURL = 'https://tequiladev.space'
 Axios.defaults.headers.get['Content-Type'] = 'application/json';
 Axios.defaults.headers.post['Content-Type'] = 'application/json';
 
